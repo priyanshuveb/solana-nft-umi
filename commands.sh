ea1e682e389b02f8c73e09d23b4d8899952a8858
priyanshuveb <priyanshubindal@gmail.com>
2024-05-30 23:24:06 +0530

    add

---
+++ commands.sh
@@ -0,0 +1,11 @@
+#pubkey: i5QSzYrHDuB2BCR69yoMJG3bMe64iPqsCcvMSSJDYA8
+#seed: next glide arrest lesson achieve remain novel unfair anxiety layer patient early
+solana-keygen new --no-bip39-passphrase --outfile ./Owner.json
+
+solana config set --keypair ./Owner.json
+solana address
+solana config set --url https://api.devnet.solana.com
+solana config get
+
+solana airdrop 5
+solana balance
