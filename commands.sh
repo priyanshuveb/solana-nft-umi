b1d6b3a902d6aa00c5f6be53111b130ebc936a87
priyanshuveb <priyanshubindal@gmail.com>
2024-06-04 01:32:07 +0530

    few tweaks

--- commands.sh
+++ commands.sh
@@ -1,6 +1,10 @@
 #pubkey: i5QSzYrHDuB2BCR69yoMJG3bMe64iPqsCcvMSSJDYA8
 #seed: next glide arrest lesson achieve remain novel unfair anxiety layer patient early
 solana-keygen new --no-bip39-passphrase --outfile ./owner.json
+#pubkey: pA1biUsXcVPpUAGHYXcrPrU5ncWwJVqWuPRFAvWdgHF
+#seed: hollow purity evidence napkin merry pause fever pioneer claw pill elite meadow
+solana-keygen new --no-bip39-passphrase --outfile ./creator.json
+
 
 solana config set --keypair ./owner.json
 solana address
